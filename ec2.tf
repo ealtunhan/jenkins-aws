d9be1a7020178979dc7dcc981f1a75dc2e3d14c8
ealtunhan <ealtunhan@gmail.com>
2023-03-15 17:28:57 -0400

    Added gitHub APIs for webhook

--- ec2.tf
+++ ec2.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "ec2_security_group" {
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0", "192.30.252.0/22", "185.199.108.0/22", "140.82.112.0/20", "143.55.64.0/20",]
   }
 
   # allow access on port 22
@@ -49,7 +49,7 @@ resource "aws_security_group" "ec2_security_group" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0", "192.30.252.0/22", "185.199.108.0/22", "140.82.112.0/20", "143.55.64.0/20",]
   }
 
   egress {
